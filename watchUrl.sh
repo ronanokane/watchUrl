2d9569c3cb1c4cb58b6e5850216e575be1aac468
ronanokane <ronan.okane@gmail.com>
2022-07-07 01:28:13 +0100

    Added cookies support

--- watchUrl.sh
+++ watchUrl.sh
@@ -9,12 +9,13 @@ git_update(){
 }
 
 getPageElement(){
-	downloadedpage=$(curl $1 2> /dev/null) || { echo "Invalid url $1" ; exit 1; }
+	downloadedpage=$(curl $(if [ $cookies != "" ]; then echo -b "\"$cookies\" "; fi)-L $1 2> /dev/null) || { echo "Invalid url $1" ; exit 1; }
 	element=$(xmllint --html --xpath $2 <(echo $downloadedpage) 2> /dev/null) || { echo 'Parse error'; exit 1; } 
 }
 
 usage(){
 	echo "Usage: $1 <url> <xPath_element>"
+	echo "Usage: $1 <url> <xPath> < cookiefile"
 	echo "Usage: $1"
 	echo
 	echo "Note: Initial use requires 2 args"
@@ -23,6 +24,8 @@ usage(){
 	exit 1
 }
 
+read -t 1 cookies
+
 if [ $# -eq 2 ]; then
 	url=$1
 	xpath=$2
@@ -30,6 +33,7 @@ elif [ $# -eq 0 ]; then
 	if [ -e "config.json" ]; then
 		url=$(cat config.json | jq '.url' | tr -d '"')
 		xpath=$(cat config.json | jq '.xpath' | tr -d '"')
+		cookies=$(cat config.json | jq '.cookies' | tr -d '"')
 	else
 		usage $0
 	fi
@@ -42,7 +46,7 @@ if [ ! -e "pagePart.html" ]; then
 	echo $element > pagePart.html
 	git init 2> /dev/null
 	git_update 'initial_commit'
-	jq -n "{url: \"$1\", xpath: \"$2\"}" > config.json
+	jq -n "{url: \"$1\", xpath: \"$2\", cookies: \"$cookies\"}" > config.json
 fi
 
 while :
