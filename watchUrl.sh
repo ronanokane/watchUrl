27268dbc347fe51f1d10c7e21e210bda6f75c0c3
ronanokane <ronan.okane@gmail.com>
2023-02-20 19:23:45 +0000

    reinstate error messages

--- watchUrl.sh
+++ watchUrl.sh
@@ -51,7 +51,7 @@ fi
 while :
 do
 	sleep 10
-	getPageElement "$url" "$xpath" 2> /dev/null || continue
+	getPageElement "$url" "$xpath" || continue
 
 	if [ $(md5sum "pageExtract" | awk '{print $1}') != $(echo "$extract" | md5sum | awk '{print $1}') ]; then
 		echo "$extract" > "pageExtract"
